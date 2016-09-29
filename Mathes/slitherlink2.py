bad3b17f2e26829cf5d7288c23ff5062cfce0cf0
koreanwglasses <koreanwglasses@gmail.com>
2016-09-29 00:11:14 -0400

    Updated slitherlinks

--- Mathes/slitherlink2.py
+++ Mathes/slitherlink2.py
@@ -336,9 +336,9 @@ def get_candidates(solution):
         parity_pass_0(link, parity)
         parity_pass_1(link, parity, known_edges)
 
-        # print to_string(solution)
-        # print
-        # time.sleep(.1)
+        print to_string(solution)
+        print
+        time.sleep(.1)
 
         if link[0] == link[-1]:
             sub = under(edge_counts)
@@ -380,6 +380,7 @@ def get_candidates(solution):
         new_known_edges = known_edges[:]
 
         candidates.append([newlink, newedge, newparity, new_known_edges])
+    random.shuffle(candidates)
     return candidates
 
 def to_string(solution):
